3e621d328ffd8c41a60f90a1afcd72d986ffe66c
unknown <miller.s.jeffrey@gmail.com>
2021-09-03 16:48:42 -0500

    Adding a settings dialog and animation; begin implementation of settings dialog functionality

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour {
+  public Animator StartButton;
+  public Animator SettingsButtons;
+  public Animator SettingsDialog;
+
+  public void CloseSettings() {
+    StartButton.SetBool("isHidden", false);
+    SettingsButtons.SetBool("isHidden", false);
+    SettingsDialog.SetBool("isHidden", true);
+  }
+  
+  public void OpenSettings() {
+    StartButton.SetBool("isHidden", true);
+    SettingsButtons.SetBool("isHidden", true);
+    SettingsDialog.SetBool("isHidden", false);
+  }
+
   public void StartGame() {
     SceneManager.LoadScene("RocketMouse");
   }
