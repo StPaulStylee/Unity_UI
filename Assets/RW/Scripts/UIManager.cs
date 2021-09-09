696f6f2c113bd4ec4049c6e70421ebbe36083f79
unknown <miller.s.jeffrey@gmail.com>
2021-09-09 07:04:50 -0500

    Add a sliding menu that uses masking; update points text in the game to use new UI system; create a new restart dialogue

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -7,6 +7,8 @@ public class UIManager : MonoBehaviour {
   public Animator StartButton;
   public Animator SettingsButtons;
   public Animator SettingsDialog;
+  public Animator ContentPanel;
+  public Animator GearIcon;
 
   public void CloseSettings() {
     StartButton.SetBool("isHidden", false);
@@ -22,5 +24,11 @@ public class UIManager : MonoBehaviour {
 
   public void StartGame() {
     SceneManager.LoadScene("RocketMouse");
+  }
+
+  public void ToggleMenu() {
+    bool isHidden = ContentPanel.GetBool("isHidden");
+    ContentPanel.SetBool("isHidden", !isHidden);
+    GearIcon.SetBool("isRotating", isHidden);
   }
 }
